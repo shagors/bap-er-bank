e69d07a1d7d4a61c315f74dd3910b30e120eb05d
shagors <shajjadshagor@gmail.com>
2022-02-10 04:52:50 +0600

    deposit button add and when money that time deposit update

--- js/banking.js
+++ js/banking.js
@@ -1,5 +1,20 @@
 // deposit and withdraw set up
 
 document.getElementById("deposit-button").addEventListener("click", function(e){
-    console.log("button clicked");
+    // get the amount deposited
+    const depositInput = document.getElementById("deposit-input");
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+
+    const depositTotal = document.getElementById("deposit-total");
+
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+
+    
+
+    // clear deposit input field
+    depositInput.value = "";
 });
